44586fbad5b2465160354662d0d5b58692f26fa7
Nuno135 <nuno.n@post.com>
2018-02-04 22:38:54 +0100

    Fixed util in cat.js

--- src/methods/cat.js
+++ src/methods/cat.js
@@ -1,15 +1,21 @@
 var Execi = require('../execi');
 var fs = require('fs');
-var spawn = require('../utils/spawn');
+var utils = require('../utils');
 
 Execi.prototype.cat = function(data) {
+    
     if (typeof data !== 'string')
         throw new Error('Invalid string');
 
     if (!fs.existsSync(data)) {
+        
         err = 'cat: no such file: ' + data;
+        
         console.log(err)
+        
     } else {
-        spawn('cat', data);
+        
+        utils.spawn('cat', data);
+        
     }
 }

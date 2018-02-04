3eeaec1eaf5cea46cca419685025014dcfc7ae1a
Nuno135 <nuno.n@post.com>
2018-02-04 15:17:23 +0100

    Updated ls and cd

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var Execi = require('./src/execi');
 module.exports = {
   // Methods
   exec: require('./src/methods/exec'),
-  echo: require('./src/methods/echo')
+  echo: require('./src/methods/echo'),
+  ls: require('./src/methods/ls'),
+  cd: require('./src/methods/cd')
 };
 
 module.exports = new Execi();
